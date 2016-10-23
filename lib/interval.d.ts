673ba00441374fc895cd1c5a59e916d341e7dc91
John J. Glynn IV <jjg1914@rit.edu>
2016-10-23 15:48:04 -0400

    v0.2.0

--- lib/interval.d.ts
+++
@@ -1,7 +0,0 @@
-import * as Immutable from "immutable";
-import { Callback } from "./runtime";
-export default function Interval<T>(rate: number, cb: Callback<T>): void;
-export declare class Event extends Immutable.Map<string, any> {
-    t: number;
-    dt: number;
-}
--- lib/timeline.d.ts
+++
@@ -1,3 +0,0 @@
-import IO from "./io";
-export declare type Pair<T> = [number, (t: T) => IO<T>];
-export default function Timeline<T>(timeline: Pair<T>[]): IO<T>;
