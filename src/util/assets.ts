2ef570b71efc0b4769a3ae22ccc81f88e8cf1138
John J. Glynn IV <jjg1914@rit.edu>
2017-09-19 21:17:34 -0400

    iterable assets

--- src/util/assets.ts
+++ src/util/assets.ts
@@ -22,4 +22,28 @@ export class Assets {
         return Stage.unserialize(data.data);
     }
   }
+
+  paths(): string[] {
+    const rval: string[] = [];
+
+    for (let e in this._config) {
+      if (this._config.hasOwnProperty(e) && this._config[e].type === "path") {
+        rval.push(e);
+      }
+    }
+
+    return rval;
+  }
+
+  stages(): string[] {
+    const rval: string[] = [];
+
+    for (let e in this._config) {
+      if (this._config.hasOwnProperty(e) && this._config[e].type === "stage") {
+        rval.push(e);
+      }
+    }
+
+    return rval;
+  }
 }
