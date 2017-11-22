51e649f93548da6462d324693caebf03a058ba4c
John J. Glynn IV <jjg1914@rit.edu>
2017-11-21 21:19:52 -0500

    test coverage reporting

--- test/test.ts
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai';
-import 'mocha';
-
-describe('Hello function', () => {
-  it('should return hello world', () => {
-    expect('Hello World!').to.equal('Hello World!');
-  });
-});
