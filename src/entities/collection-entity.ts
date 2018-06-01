c859420aa389d37b95d5ac5f8345c6f881397366
John J. Glynn IV <jjg1914@rit.edu>
2018-05-31 21:14:46 -0400

    Guaranetee inorder transversal of collection-entity

--- src/entities/collection-entity.ts
+++ src/entities/collection-entity.ts
@@ -7,9 +7,17 @@ import {
   EntityChildEvent,
 } from "../events/entity-event";
 
+interface Node {
+  entity: Entity;
+  next?: number;
+  prev?: number;
+}
+
 export class CollectionEntity extends BaseEntity {
-  private _collection: { [id: number ]: Entity };
+  private _collection: { [id: number ]: Node };
   private _length: number;
+  private _head?: number;
+  private _tail?: number;
 
   constructor() {
     super();
@@ -17,8 +25,13 @@ export class CollectionEntity extends BaseEntity {
     this._length = 0;
 
     this.last((event: string, data: any) => {
-      for (let e in this._collection) {
-        if (this._collection[e].send(event, data)) {
+      let i = this._head;
+
+      while (i !== undefined) {
+        const e = this._collection[i].entity;
+        i = this._collection[i].next;
+
+        if (e.send(event, data)) {
           return true;
         }
       }
@@ -41,8 +54,23 @@ export class CollectionEntity extends BaseEntity {
 
   put(entity: Entity): this {
     if (entity.parent === undefined) {
-      this._collection[entity.id] = entity;
+      const node = { entity: entity } as Node;
+
+      this._collection[entity.id] = node;
       entity.parent = this;
+
+      if (this._head === undefined) {
+        this._head = entity.id;
+      } else {
+        node.prev = this._tail;
+      }
+
+      if (this._tail !== undefined) {
+        this._collection[this._tail].next = entity.id;
+      }
+
+      this._tail = entity.id;
+
       this._length += 1;
 
       entity.send("enter", new EntityChildEvent("enter"));
@@ -54,6 +82,28 @@ export class CollectionEntity extends BaseEntity {
   remove(entity: Entity): this {
     if (entity.parent === this) {
       entity.send("exit", new EntityChildEvent("exit"));
+
+      const node = this._collection[entity.id];
+
+      if (node.prev !== undefined) {
+        if (node.next !== undefined) {
+          this._collection[node.prev].next = node.next;
+        } else {
+          delete this._collection[node.prev].next;
+        }
+      } else if (node.next !== undefined) {
+        this._head = node.next;
+      }
+
+      if (node.next !== undefined) {
+        if (node.prev !== undefined) {
+          this._collection[node.prev].prev = node.prev;
+        } else {
+          delete this._collection[node.next].prev;
+        }
+      } else if (node.prev !== undefined) {
+        this._tail = node.prev;
+      }
 
       delete entity.parent;
       delete this._collection[entity.id];
